ecce6a5eec34e7eed5c97a590350c1e3ee588b93
Paul <paulvjarca@gmail.com>
2022-05-31 23:51:11 +0300

    table and drag&drop file upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
@@ -15,3 +10,4 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
